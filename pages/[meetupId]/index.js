ae9c963af219b20b7fa46f0ba3e08b9c9ce8692b
skabaza <skabaza46@gmail.com>
2022-11-24 05:37:15 -0500

    enabled fallback, so that pages can be generated on demand and cache it on meetup details page.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -38,7 +38,7 @@ export const getStaticPaths = async () =>{
     client.close();
 
     return {
-        fallback: true,
+        fallback: 'blocking',
         paths: meetups.map(meetup => ({
             params: { meetupId: meetup._id.toString()}
          })),
